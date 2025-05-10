8c72c91ae206b567cd022ba30804efc839559462
Atulbedwal <atulbedwal69@gmail.com>
2025-05-10 18:00:48 +0530

    Changes

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: ["http://localhost:5173",
-    "https://coding-blocks-iota.vercel.app/"
+    "https://coding-blocks-iota.vercel.app"
    ],
   credentials: true               
 }));
