7746e953599f84039fba7ed1977c9bc082555ded
Atulbedwal <atulbedwal69@gmail.com>
2025-05-10 19:22:11 +0530

    Updates

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
     setIsLoading(true)
 
     try {
-      const response = await api.post('/auth/login', { email, password })
+      const response = await api.post('/api/auth/login', { email, password })
       localStorage.setItem('token', response.data.token)
       navigate('/dashboard') // Redirect to DashboardPage
     } catch (err) {
