8ba5d5fc9b9880ddbde8ce43fce746533583d4fa
Atulbedwal <atulbedwal69@gmail.com>
2025-05-10 18:47:00 +0530

    Changes

--- server/src/middleware/authMiddlewares.js
+++ server/src/middleware/authMiddlewares.js
@@ -1,21 +1,21 @@
-// // /middlewares/authMiddleware.js
+// /middlewares/authMiddleware.js
 
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-// const verifyToken = (req, res, next) => {
-//   const token = req.headers['authorization']?.split(' ')[1]; // Get token from Authorization header
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization']?.split(' ')[1]; // Get token from Authorization header
   
-//   if (!token) {
-//     return res.status(403).json({ message: 'No token provided' });
-//   }
+  if (!token) {
+    return res.status(403).json({ message: 'No token provided' });
+  }
 
-//   jwt.verify(token, 'your-secret-key', (err, decoded) => {
-//     if (err) {
-//       return res.status(401).json({ message: 'Invalid or expired token' });
-//     }
-//     req.userId = decoded.id; // Store user ID in the request
-//     next();
-//   });
-// };
+  jwt.verify(token, 'your-secret-key', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid or expired token' });
+    }
+    req.userId = decoded.id; // Store user ID in the request
+    next();
+  });
+};
 
-// module.exports = verifyToken;
+module.exports = verifyToken;
